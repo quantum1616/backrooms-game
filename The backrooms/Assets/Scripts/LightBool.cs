9894764cdc72b3e14bcc24fbfa2e5c57273ff9a4
fryhecker <142638967+fryhecker@users.noreply.github.com>
2023-08-20 16:31:47 -0600

    Battery Update And New Moment System

--- The backrooms/Assets/Scripts/LightBool.cs
+++ The backrooms/Assets/Scripts/LightBool.cs
@@ -7,10 +7,20 @@ public class LightBool : MonoBehaviour
     public Light FlashlightSpotLight;
 
     public bool FlashlightEnabled = true;
+
+    public float battarytime = 100000f;
+
+    public float depletionspeed = 1f;
+
+    public float lightturnoffnumber = 0f;
+
+    public float BattaryCalculation;
+
+
    
     void Start()
     {
-        
+       FlashlightEnabled = false;
      
 
 
@@ -32,11 +42,20 @@ public class LightBool : MonoBehaviour
         if (FlashlightEnabled == true)
         {
             FlashlightSpotLight.enabled = true;
+
+            battarytime = battarytime - depletionspeed * Time.deltaTime;
         }
         if (FlashlightEnabled == false)
         {
             FlashlightSpotLight.enabled = false;
         }
+        if (battarytime <= 0)
+        {
+            FlashlightSpotLight.enabled = false;
+        }
+
+        BattaryCalculation = battarytime / 100;
+        BattaryCalculation = Mathf.Round(BattaryCalculation * Mathf.Pow(10, 0)) / Mathf.Pow(10, 0);
 
 
 
--- The backrooms/Assets/Scripts/PlayerMovement.cs
+++ The backrooms/Assets/Scripts/PlayerMovement.cs
@@ -2,86 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Transform playerCamera;
-    [SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
-    [SerializeField] bool cursorLock = true;
-    [SerializeField] float mouseSensitivity = 3.5f;
-    [SerializeField] float Speed = 6.0f;
-    [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
-    [SerializeField] float gravity = -30f;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask ground;
+    public float speed = 5f;
+    public float cameraSensitivity = 2f;
 
-    public float jumpHeight = 6f;
-    float velocityY;
-    bool isGrounded;
-
-    float cameraCap;
-    Vector2 currentMouseDelta;
-    Vector2 currentMouseDeltaVelocity;
-
-    CharacterController controller;
-    Vector2 currentDir;
-    Vector2 currentDirVelocity;
-    Vector3 velocity;
+    private CharacterController controller;
+    private Transform cameraTransform;
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotX, rotY;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
-        if (cursorLock)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = true;
-        }
+        cameraTransform = Camera.main.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        UpdateMouse();
-        UpdateMove();
-    }
+        // Camera rotation
+        rotX += Input.GetAxis("Mouse X") * cameraSensitivity;
+        rotY += Input.GetAxis("Mouse Y") * cameraSensitivity;
+        rotY = Mathf.Clamp(rotY, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(-rotY, 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, rotX, 0f);
 
-    void UpdateMouse()
-    {
-        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        // Movement
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
 
-        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
-
-        cameraCap -= currentMouseDelta.y * mouseSensitivity;
-
-        cameraCap = Mathf.Clamp(cameraCap, -90.0f, 90.0f);
-
-        playerCamera.localEulerAngles = Vector3.right * cameraCap;
-
-        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
-    }
-
-    void UpdateMove()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, ground);
-
-        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        targetDir.Normalize();
-
-        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
-
-        velocityY += gravity * 2f * Time.deltaTime;
-
-        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * Speed + Vector3.up * velocityY;
-
-        controller.Move(velocity * Time.deltaTime);
-
-        if (isGrounded && Input.GetButtonDown("Jump"))
-        {
-            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        if (isGrounded! && controller.velocity.y < -1f)
-        {
-            velocityY = -8f;
-        }
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
